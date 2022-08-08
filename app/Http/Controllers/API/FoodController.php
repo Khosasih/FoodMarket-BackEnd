23a774272c41c1d95271d45e33a3a0c27337b6ab
Khosasih <adammaulanakh1@gmail.com>
2022-08-09 00:42:03 +0700

    fixed foodController

--- app/Http/Controllers/API/FoodController.php
+++ app/Http/Controllers/API/FoodController.php
@@ -23,62 +23,50 @@ class FoodController extends Controller
         $rate_from = $request->input('rate_from');
         $rate_to = $request->input('rate_to');
 
-        if ($id) 
-        {
+        if ($id) {
             $food = Food::find($id);
 
-            if ($food) 
-            {
+            if ($food)
                 return ResponseFormatter::success(
                     $food,
                     'Data Produk berhasil diambil'
                 );
-            }
             else
-            {
                 return ResponseFormatter::error(
                     null,
                     'Data produk tidak ada',
                     404
                 );
-            }
-
-            $food = Food::query();
-            if ($name) 
-            {
-                $food->where('name', 'like', '%' . $name . '%');
-            }
-
-            if ($types) 
-            {
-                $food->where('types', 'like', '%' . $types . '%');
-            }
-
-            if ($price_from) 
-            {
-                $food->where('price_from', '>=', $price_from);
-            }
-
-            if ($price_to) 
-            {
-                $food->where('price_to', '<=', $price_to);
-            }
-
-            if ($rate_from) 
-            {
-                $food->where('rate_from', '>=', $rate_from);
-            }
-
-            if ($rate_to) 
-            {
-                $food->where('rate_to', '<=', $rate_to);
-            }
-
-            return ResponseFormatter::success(
-                $food->paginate($limit),
-                'Data list product berhasil diambil'
-            );
-
         }
+
+        $food = Food::query();
+        if ($name)
+            $food->where('name', 'like', '%' . $name . '%');
+
+
+        if ($types)
+            $food->where('types', 'like', '%' . $types . '%');
+
+
+        if ($price_from)
+            $food->where('price_from', '>=', $price_from);
+
+
+        if ($price_to)
+            $food->where('price_to', '<=', $price_to);
+
+
+        if ($rate_from)
+            $food->where('rate_from', '>=', $rate_from);
+
+
+        if ($rate_to)
+            $food->where('rate_to', '<=', $rate_to);
+
+
+        return ResponseFormatter::success(
+            $food->paginate($limit),
+            'Data list product berhasil diambil'
+        );
     }
 }
