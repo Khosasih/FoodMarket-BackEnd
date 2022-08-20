b3eacd08346e823dc87d6c3ac3e0e8efc5dcbdf6
Khosasih <adammaulanakh1@gmail.com>
2022-08-21 01:29:43 +0700

    show photo food

--- resources/views/food/edit.blade.php
+++ resources/views/food/edit.blade.php
@@ -82,7 +82,7 @@
                         Types
                     </label>
                     <input value="{{ old('types') ?? $item->types }}" name="types" class="appearance-none block w-full bg-gray-200 text-gray-700 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="types">
-                    <p class="text-gray-600 text-xs italic">contoh: recommended,popular,new_food</p>
+                    <p class="text-gray-600 text-xs italic">contoh: recommended,popular,new food</p>
                     </div>
                 </div>
                 <div class="flex flex-wrap -mx-3 mb-6">
