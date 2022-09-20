6ef6af6101578f2f9372a6d1c1f5ab96046febc9
Khosasih <adammaulanakh1@gmail.com>
2022-09-21 00:39:27 +0700

    adding table types and relationship

--- resources/views/food/create.blade.php
+++ resources/views/food/create.blade.php
@@ -104,7 +104,6 @@
                                 for="grid-last-name">
                                 Types
                             </label>
-                            {{-- tinggal ini  --}}
                             <input value="{{ old('types_id') }}" name="types_id"
                                 class="appearance-none block w-full bg-gray-200 text-gray-700 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                 id="grid-last-name" type="text" placeholder="types">
